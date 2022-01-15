03fc1b79aa8385252ba5daa61ada45dfafc24b97
layup <tlaymbss@gmail.com>
2022-01-14 16:36:44 -0800

    updating package files

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,28 @@
 const path = require('path');
 
 module.exports = {
-    entry:'./src/app.js', 
+    mode: 'development', // configuration needs to be explicitly state or a lot of performance warnings 
+    entry: './src/app.js', // entry -> output
     output: {
-        path:path.join(__dirname, 'public'), 
-        filename:'bundle.js'
-    }, 
-    module: {
-        rules:[{
-            loader: 'babel-loader', 
-            test:/\.js$/,
-            exclude:/nodes_modules/
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    module: { //loader
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
         }]
     },
-    devtool:'eval-cheap-module-source-map',
-    devServer:{
+    devtool: 'cheap-module-source-map', // vs cheap-module=-eval-source-map in webpack
+    devServer: {
         contentBase: path.join(__dirname, 'public')
     }
 };
-
-
-
-//loader = allows to customize how we load files 
