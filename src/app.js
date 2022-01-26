8ab2eec4ce311256baef943edab97ff659bba49e
layup <tlaymbss@gmail.com>
2022-01-25 21:16:31 -0800

    app complete

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
+
+import 'normalize.css/normalize.css'; //all running on the same base from each browsers 
 import './styles/styles.scss'; 
 
 
